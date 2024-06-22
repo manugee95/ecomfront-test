0801ca33d856a3d2609b2119a9e2bc64f1e85ccb
manugee <manugeetech@gmail.com>
2024-06-22 16:47:59 +0100

    setheader

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,43 +2,26 @@ const Product = require("../models/product");
 const { validateProduct } = require("../validator");
 
 exports.createProduct = async (req, res) => {
-  // const { error } = validateProduct(req.body);
-  // if (error) {
-  //   res.json(error.details[0].message);
-  // } else {
-  //   try {
-  //     const product = new Product({
-  //       category: req.body.category,
-  //       name: req.body.name,
-  //       img: req.file.path,
-  //       price: req.body.price,
-  //       featured: req.body.featured,
-  //       topSelling: req.body.topSelling,
-  //     });
+  const { error } = validateProduct(req.body);
+  if (error) {
+    res.json(error.details[0].message);
+  } else {
+    try {
+      const product = new Product({
+        category: req.body.category,
+        name: req.body.name,
+        img: req.file.path,
+        price: req.body.price,
+        featured: req.body.featured,
+        topSelling: req.body.topSelling,
+      });
 
-  //     const productItem = await product.save();
-  //     res.setHeader('Content-Type', 'application/json')
-  //     res.json(productItem);
-  //   } catch (error) {
-  //     console.log({ message: error.message });
-  //   }
-  // }
-
-  try {
-    const product = new Product({
-      category: req.body.category,
-      name: req.body.name,
-      img: req.file.path,
-      price: req.body.price,
-      featured: req.body.featured,
-      topSelling: req.body.topSelling,
-    });
-
-    const productItem = await product.save();
-    res.setHeader('Content-Type', 'application/json')
-    res.json(productItem);
-  } catch (error) {
-    console.log({ message: error.message });
+      const productItem = await product.save();
+      res.setHeader('Content-Type', 'application/json')
+      res.json(productItem);
+    } catch (error) {
+      console.log({ message: error.message });
+    }
   }
 };
 
